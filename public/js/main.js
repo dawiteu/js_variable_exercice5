cbe9ca740ad4f092638b07d9549af569c1f7763e
DawiT <Dawid@Tararuj.be>
2020-12-28 16:51:51 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+let n1 = parseInt(prompt("entre le nombre 1 ")); 
+let n2 = parseInt(prompt("entre le nombre 2 "));
+
+console.log(`${n1} + ${n2} = ${n1+n2}`);
+console.log(`${n1} x ${n2} = ${n1*n2}`);
+console.log(`${n1} - ${n2} = ${n1-n2}`);
+console.log(`${n1} : ${n2} = ${n1/n2}`);
+
+
+let age = parseInt(prompt("Entre ton age")); 
+let nom = prompt("entre ton prenom"); 
+
+console.log(`${nom} a ${age} ans cette annee.`); 
+
+let dans = 3; 
+console.log(`${nom} aura ${age+dans} ans dans ${dans} ans`);
